88479301e024888050bb9b729a15332fe8529fbf
h82258652 <h82258652@hotmail.com>
2018-01-31 08:37:21 +0800

    update

--- NSUI/NSUI.Wpf/Controls/NSCircleFocusVisual.cs
+++ NSUI/NSUI.Wpf/Controls/NSCircleFocusVisual.cs
@@ -1,95 +1,12 @@
-﻿using System.Diagnostics;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Media.Animation;
+﻿using System.Windows;
 
 namespace NSUI.Controls
 {
-    [TemplatePart(Name = VisualElementTemplateName, Type = typeof(FrameworkElement))]
-    public class NSCircleFocusVisual : Control, INSFocusVisual
+    public class NSCircleFocusVisual : NSFocusVisualBase
     {
-        private const string VisualElementTemplateName = "PART_VisualElement";
-
-        private FrameworkElement _visualElement;
-
         static NSCircleFocusVisual()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(NSCircleFocusVisual), new FrameworkPropertyMetadata(typeof(NSCircleFocusVisual)));
-        }
-
-        public override void OnApplyTemplate()
-        {
-            base.OnApplyTemplate();
-
-            _visualElement = (FrameworkElement)GetTemplateChild(VisualElementTemplateName);
-            Debug.Assert(_visualElement != null);
-            ((Storyboard)_visualElement.Resources["VisualBrushStoryboard"]).Begin();
-        }
-
-        public void ShakeDown()
-        {
-            if (_visualElement == null)
-            {
-                return;
-            }
-
-            var storyboard = new Storyboard();
-            var animation = new DoubleAnimation()
-            {
-                From = 0,
-                To = Constants.FocusVisualShakeToValue,
-                Duration = Constants.FocusVisualShakeDuration
-            };
-            storyboard.Children.Add(animation);
-            storyboard.Begin();
-
-            // TODO
-        }
-
-        public void ShakeLeft()
-        {
-            if (_visualElement == null)
-            {
-                return;
-            }
-
-            var storyboard = new Storyboard();
-            storyboard.Begin();
-
-            // TODO
-        }
-
-        public void ShakeRight()
-        {
-            if (_visualElement == null)
-            {
-                return;
-            }
-
-            var storyboard = new Storyboard();
-            var animation = new DoubleAnimation()
-            {
-                From = 0
-            };
-            storyboard.Children.Add(animation);
-            storyboard.Begin();
-
-            // TODO
-        }
-
-        public void ShakeUp()
-        {
-            if (_visualElement == null)
-            {
-                return;
-            }
-
-            var storyboard = new Storyboard();
-            var animation = new DoubleAnimation();
-            storyboard.Children.Add(animation);
-            storyboard.Begin();
-
-            // TODO
         }
     }
 }
---
+++ NSUI/NSUI.Wpf/Controls/NSFocusVisualBase.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
+namespace NSUI.Controls
+{
+    [TemplatePart(Name = VisualElementTemplateName, Type = typeof(FrameworkElement))]
+    public class NSFocusVisualBase : Control, INSFocusVisual
+    {
+        private const string VisualElementTemplateName = "PART_VisualElement";
+
+        private FrameworkElement _visualElement;
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            _visualElement = (FrameworkElement)GetTemplateChild(VisualElementTemplateName);
+            if (_visualElement != null)
+            {
+                ((Storyboard)_visualElement.Resources["VisualBrushStoryboard"]).Begin();
+            }
+        }
+
+        public void ShakeDown()
+        {
+            if (_visualElement == null)
+            {
+                return;
+            }
+
+            var storyboard = new Storyboard();
+            var animation = new DoubleAnimationUsingKeyFrames();
+            animation.KeyFrames.Add(new LinearDoubleKeyFrame()
+            {
+                KeyTime = TimeSpan.FromSeconds(0),
+                Value = 0
+            });
+            animation.KeyFrames.Add(new LinearDoubleKeyFrame()
+            {
+                KeyTime = TimeSpan.FromSeconds(0.4),
+                Value = 8
+            });
+            animation.KeyFrames.Add(new LinearDoubleKeyFrame()
+            {
+                KeyTime = TimeSpan.FromSeconds(0.8),
+                Value = -4
+            });
+            animation.KeyFrames.Add(new LinearDoubleKeyFrame()
+            {
+                KeyTime = TimeSpan.FromSeconds(1.2),
+                Value = 0
+            });
+            Storyboard.SetTarget(animation, _visualElement);
+            Storyboard.SetTargetProperty(animation, new PropertyPath("(UIElement.RenderTransform).(TranslateTransform.Y)"));
+            storyboard.Children.Add(animation);
+            storyboard.Begin();
+        }
+
+        public void ShakeLeft()
+        {
+            if (_visualElement == null)
+            {
+                return;
+            }
+
+            var storyboard = new Storyboard();
+            var animation = new DoubleAnimationUsingKeyFrames();
+            animation.KeyFrames.Add(new LinearDoubleKeyFrame()
+            {
+                KeyTime = TimeSpan.FromSeconds(0),
+                Value = 0
+            });
+            animation.KeyFrames.Add(new LinearDoubleKeyFrame()
+            {
+                KeyTime = TimeSpan.FromSeconds(0.4),
+                Value = -8
+            });
+            animation.KeyFrames.Add(new LinearDoubleKeyFrame()
+            {
+                KeyTime = TimeSpan.FromSeconds(0.8),
+                Value = 4
+            });
+            animation.KeyFrames.Add(new LinearDoubleKeyFrame()
+            {
+                KeyTime = TimeSpan.FromSeconds(1.2),
+                Value = 0
+            });
+            Storyboard.SetTarget(animation, _visualElement);
+            Storyboard.SetTargetProperty(animation, new PropertyPath("(UIElement.RenderTransform).(TranslateTransform.X)"));
+            storyboard.Children.Add(animation);
+            storyboard.Begin();
+        }
+
+        public void ShakeRight()
+        {
+            if (_visualElement == null)
+            {
+                return;
+            }
+
+            var storyboard = new Storyboard();
+            var animation = new DoubleAnimationUsingKeyFrames();
+            animation.KeyFrames.Add(new LinearDoubleKeyFrame()
+            {
+                KeyTime = TimeSpan.FromSeconds(0),
+                Value = 0
+            });
+            animation.KeyFrames.Add(new LinearDoubleKeyFrame()
+            {
+                KeyTime = TimeSpan.FromSeconds(0.4),
+                Value = 8
+            });
+            animation.KeyFrames.Add(new LinearDoubleKeyFrame()
+            {
+                KeyTime = TimeSpan.FromSeconds(0.8),
+                Value = -4
+            });
+            animation.KeyFrames.Add(new LinearDoubleKeyFrame()
+            {
+                KeyTime = TimeSpan.FromSeconds(1.2),
+                Value = 0
+            });
+            Storyboard.SetTarget(animation, _visualElement);
+            Storyboard.SetTargetProperty(animation, new PropertyPath("(UIElement.RenderTransform).(TranslateTransform.X)"));
+            storyboard.Children.Add(animation);
+            storyboard.Begin();
+        }
+
+        public void ShakeUp()
+        {
+            if (_visualElement == null)
+            {
+                return;
+            }
+
+            var storyboard = new Storyboard();
+            var animation = new DoubleAnimationUsingKeyFrames();
+            animation.KeyFrames.Add(new LinearDoubleKeyFrame()
+            {
+                KeyTime = TimeSpan.FromSeconds(0),
+                Value = 0
+            });
+            animation.KeyFrames.Add(new LinearDoubleKeyFrame()
+            {
+                KeyTime = TimeSpan.FromSeconds(0.4),
+                Value = -8
+            });
+            animation.KeyFrames.Add(new LinearDoubleKeyFrame()
+            {
+                KeyTime = TimeSpan.FromSeconds(0.8),
+                Value = 4
+            });
+            animation.KeyFrames.Add(new LinearDoubleKeyFrame()
+            {
+                KeyTime = TimeSpan.FromSeconds(0),
+                Value = 0
+            });
+            Storyboard.SetTarget(animation, _visualElement);
+            Storyboard.SetTargetProperty(animation, new PropertyPath("(UIElement.RenderTransform).(TranslateTransform.Y)"));
+            storyboard.Children.Add(animation);
+            storyboard.Begin();
+        }
+    }
+}
