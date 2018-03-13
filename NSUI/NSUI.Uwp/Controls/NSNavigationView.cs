8eb1fefb23b75d25736349e6e922f1e7e2552ad3
h82258652 <h82258652@hotmail.com>
2018-03-13 15:57:16 +0800

    update

---
+++ NSUI/NSUI.Uwp/Controls/NSNavigationView.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace NSUI.Controls
+{
+    public class NSNavigationView : NavigationView
+    {
+        public NSNavigationView()
+        {
+            DefaultStyleKey = typeof(NSNavigationView);
+        }
+    }
+}
---
+++ NSUI/NSUI.Wpf/Controls/NSNavigationView.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace NSUI.Controls
+{
+    [TemplatePart(Name = MenuItemsHostTemplateName, Type = typeof(ListView))]
+    public class NSNavigationView : ContentControl
+    {
+        public static readonly DependencyProperty MenuItemsProperty = DependencyProperty.Register(nameof(MenuItems), typeof(IList<object>), typeof(NSNavigationView), new PropertyMetadata(default(IList<object>)));
+        public static readonly DependencyProperty MenuItemsSourceProperty = DependencyProperty.Register(nameof(MenuItemsSource), typeof(object), typeof(NSNavigationView), new PropertyMetadata(default(object)));
+
+        private const string MenuItemsHostTemplateName = "PART_MenuItemsHost";
+
+        static NSNavigationView()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(NSNavigationView), new FrameworkPropertyMetadata(typeof(NSNavigationView)));
+        }
+
+        public IList<object> MenuItems => (IList<object>)GetValue(MenuItemsProperty);
+
+        public object MenuItemsSource
+        {
+            get => GetValue(MenuItemsSourceProperty);
+            set => SetValue(MenuItemsSourceProperty, value);
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            var menuItemsHost = (ListView)GetTemplateChild(MenuItemsHostTemplateName);
+        }
+    }
+}
